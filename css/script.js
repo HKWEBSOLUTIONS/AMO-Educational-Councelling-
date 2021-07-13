b991b16190abfb8758035f62c09c8fad8c0f6a13
HKWEBSOLUTIONS <87391415+HKWEBSOLUTIONS@users.noreply.github.com>
2021-07-14 00:51:14 +0530

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,26 @@
+ocument.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    rconst readMoreBtn = deadMoreBtn.innerText = "Read More";
+  }
+});
+
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
